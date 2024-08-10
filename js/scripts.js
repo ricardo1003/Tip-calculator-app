15a00d670f2047eedff1dd78c1c64a00ae389778
unknown <ricardodonado1105@gmail.com>
2024-08-10 16:36:13 -0500

    tip calculation logic done

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ const percentageButtons = [...document.getElementsByClassName("tipPercentage")]
 const customPriceInput = [...document.getElementsByClassName("customPrice")]
 const resultTexts = [...document.getElementsByClassName("result")]
 const resetButton = document.getElementsByClassName("resetButton")[0]
+
+let selectedTip = -1
+
+let tipPerPerson = 0
+let totalTip = 0
+function calculateTip(){
+    let billCost = inputsTypeNumber[0].value
+    let tipOptions = parseInt(percentageButtons[selectedTip].getElementsByTagName("p")[0].innerHTML.replace("%",""))
+    let NofPeople = inputsTypeNumber[2].value
+    tipPerPerson = (billCost*tipOptions/100)/NofPeople
+    totalTip = ((billCost*tipOptions/100)/NofPeople)+billCost/NofPeople
+
+    console.log(tipPerPerson + " | " + totalTip)
+}
+for(let i = 0; i<percentageButtons.length;i++){
+    percentageButtons[i].addEventListener("change", ()=>{
+        selectedTip = i
+    })
+}
+
+
+calculateTip()
