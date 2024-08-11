e350c9fc427af4aae9a2afd1b90955fca7370480
unknown <ricardodonado1105@gmail.com>
2024-08-11 15:27:27 -0500

    custom input logic created

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 const inputsTypeNumber = [...document.getElementsByClassName("numberInput")]
 const percentageButtons = [...document.getElementsByClassName("tipPercentage")]
-const customPriceInput = [...document.getElementsByClassName("customPrice")]
+const customPriceInput = document.getElementById("customPrice")
 const resultTexts = [...document.getElementsByClassName("result")]
 const resetButton = document.getElementsByClassName("resetButton")[0]
 
 let selectedTip = -1
 
-// let tipPerPerson = 0
-// let totalTip = 0
-
 function calculateTip(){
     let billCost = inputsTypeNumber[0].value
-    let tipOptions = parseInt(percentageButtons[selectedTip].getElementsByTagName("p")[0].innerHTML.replace("%",""))
+    let tipOptions = 0
+    if(selectedTip < 5){
+        tipOptions = parseInt(percentageButtons[selectedTip].getElementsByTagName("p")[0].innerHTML.replace("%",""))
+    }else{
+        tipOptions = parseFloat(customPriceInput.value)
+    }
     let NofPeople = inputsTypeNumber[1].value
-
     let results = new Array(2).fill(0)
 
     results[0] = (billCost*tipOptions/100)/NofPeople
@@ -22,7 +23,6 @@ function calculateTip(){
     for(let i = 0; i<resultTexts.length;i++){
         resultTexts[i].innerHTML = "$" + (results[i]).toFixed(2)
     }
-    console.log(results)
 }
 
 let checked = new Array(3).fill(false)
@@ -35,7 +35,6 @@ for(let i = 0; i<inputsTypeNumber.length;i++){
         }
     })
 }
-
 for(let i = 0; i<percentageButtons.length;i++){
     percentageButtons[i].addEventListener("change", ()=>{
         selectedTip = i
@@ -45,6 +44,13 @@ for(let i = 0; i<percentageButtons.length;i++){
         }
     })
 }
-
-
-// calculateTip()
+customPriceInput.addEventListener("input",()=>{
+    if(customPriceInput.value != 0){
+        if(selectedTip < 5){
+            percentageButtons[selectedTip].getElementsByTagName("input")[0].checked = false
+        }
+        checked[2] = true
+        selectedTip = 5
+        calculateTip()
+    }
+})
